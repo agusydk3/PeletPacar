22547a274e6552e4e9f1919f7b3d689ce5a9cf55
WhatTheSemvak <54854607+WhatTheSemvak@users.noreply.github.com>
2019-11-09 22:34:50 +0700

    Create Pacaran.sh

---
+++ Pacaran.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+r='\033[31;2m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;2m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;2m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+bl='\33[36;1m' #BlueLight
+
+
+clear
+
+sleep 2
+echo "\033[0;32m
+______________________
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+  ║▒▒▒▒▒▒▒▒▒▒║
+ ╔════════════╗
+  MR.Revoer'S
+ ╚════════════╝
+  ║██████████╚╗
+  ║██╔══╗█╔═╗█║
+  ║██║╬╔╝█╚╗║█║
+  ║██╚═╝█║█╚╝█║
+  ╚╗█████████═╝
+     ╚╗║╠╩╩╩╩╩╝
+       ║║┈┈┈█▐█████▒.｡oO
+       ║██╠╦╦╦╗
+       ╚╗██████";
+
+sleep 2
+
+echo "Siapa Nama Mu?" | lolcat
+read -p " Input Nama : " jeneng
+
+toilet -f future "Welcome Mr.$jeneng" -w 720  -F gay
+
+toilet -f future "Di Tools Revoer'S" -w 710  -F gay
+
+sleep 0.03
+
+echo $g"##############################"
+echo $p"Tools Ini Berguna Untuk Pelet Pacar"
+echo ""
+echo $g"Habis Pelet Jan Lupa Ngewek :v"
+echo $p"##############################"
+
+echo ""
+
+sleep 2
+
+read -p "Input Nama Kamu~# " jenengku
+
+echo""
+
+sleep 1
+
+read -p "Input Nama Kekasih~# " jenengdoi
+
+echo ""
+
+sleep 1
+
+echo $bl"Tunggu Sebentar Tuan.$jenengku"
+sleep 3
+
+clear
+
+sleep 3
+
+toilet -f future Sebentar Lagi Dia -F gay
+
+echo ""
+
+toilet -f future Akan Di Pelet -F gay
+
+sleep 0.02
+
+echo ""
+
+toilet -f future Pilih Opsi Dibawah Ini -F gay
+
+sleep 1
+
+echo "1.Bulu Perindu" | lolcat
+echo "2.Dukun Cinta" | lolcat
+
+echo ""
+
+read -p "Pilih Opsi~# " ops
+
+if [ $ops = "1" ] || [ $ops = "Bulu Perindu" ]
+then
+
+figlet -f slant Bulu Puki | lolcat
+
+echo $cy"Kamu Memilih Angka $ops , Pacar Akan Tertarik Sama Lu"
+
+echo $p"Tunggu Sebentar......"
+
+sleep 5
+
+figlet -f slant Selamat :v | lolcat
+
+sleep 1
+
+toilet -f future 😘Pacar  Sudah Tergaet😘 -F gay
+
+echo ""
+
+echo $y"Silahkan Tembak Kekasih Anda"
+
+echo  ""
+
+sleep  2
+
+echo $b"Pacar Anda Yang Bernama $jenengdoi Sudah Terpelet😘"
+
+fi
+
+if [ $ops = "2" ] || [ $ops = "Dukun Cinta" ]
+then
+
+figlet -f slant Dukun Cinta | lolcat
+
+echo $cy"Kamu Memilih Angka  $ops , Pacar Akan Tertarik Sama Lu"
+
+echo $p"Tunggu Sebentar......"
+
+sleep 5
+
+figlet -f slant Selamat :v | lolcat
+
+sleep 1
+
+toilet -f future 😘Pacar  Sudah Tergaet😘 -F gay
+
+echo ""
+
+echo $y"Silahkan Tembak Kekasih Anda"
+
+echo "" 
+
+echo ""
+
+sleep  2
+
+echo $b"Pacar Anda Yang Bernama $jenengdoi Sudah Terpelet😘😘"
+
+fi
+
+
